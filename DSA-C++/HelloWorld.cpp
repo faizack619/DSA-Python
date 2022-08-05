99d1d2661be44eaacfe67ee6fcea33e113258d36
faizack619 <faizack619@gmail.com>
2022-08-05 07:30:14 +0530

    done

---
+++ DSA-C++/HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace  std ;
+
+int main(){
+
+    cout << "hello World" << endl;
+
+
+    cout << "hello World" << endl;
+
+    cout << "hello World" << endl;
+
+
+    return 0;
+
+    
+}
