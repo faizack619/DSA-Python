facbc6a74546be7f67dceece45d729b5ef218a30
faizack619 <faizack619@gmail.com>
2022-08-21 17:12:27 +0530

    PatternPratice

--- DSA/pattern/Problem-to-solve/basic pattern/Advances_Pattern/Palindromic_Pattern.py
+++ DSA/pattern/Problem-to-solve/basic pattern/Advances_Pattern/Palindromic_Pattern.py
@@ -1,10 +1,28 @@
+# Palindromic_Pattern
+    #      1
+    #     212
+    #    32123
+
+def palindromicPyramid(row):
+    #Outer Loop for each row
+ for i in range(0,row+1):
+    # 1st inner loop for printing blank spaces in each row
+    for j in range(i,row+1):
+        print("",end=" ")
+    # 2nd inner loop for printing increasing half in each row
+    for j in range(1,i+1):
+        print(j,end="")   
+     # 3rd inner loop for printing decreasing half in each row
+    for j in range(i-1,0,-1):
+        print(j,end="")       
+    # print new line character for next
+    print()   
+
+
 n=(int)(input("Enter the Number \n"))
 
-# for row in range(1,n+1) :
-#     for col in range(0,n-row):
-#         print("",end=" ")
-#     k=row
-#     for col in range()    
-#     for col in range(n-row+1) :
-#         print(row,end="") 
-#     print()         
+palindromicPyramid(n)
+
+
+
+      
---
+++ DSA/pattern/Problem-to-solve/basic pattern/Advances_Pattern/Star Pattern.py
@@ -0,0 +1,35 @@
+    #     *
+    #    ***
+    #   *****
+
+
+def palindromicPyramid(row):
+    #Outer Loop for each row
+ for i in range(0,row+1):
+    # 1st inner loop for printing blank spaces in each row
+    for j in range(i,row+1):
+        print("",end=" ")
+    # 2nd inner loop for printing increasing half in each row
+    for j in range(1,i+1):
+        print("*",end="")   
+     # 3rd inner loop for printing decreasing half in each row
+    for j in range(i-1,0,-1):
+        print("*",end="")       
+    # print new line character for next
+    print()
+#  for i in range(0,row+1):
+#     # 1st inner loop for printing blank spaces in each row
+#     for j in range(1,row+1):
+#         print("",end=" ")
+#     # 2nd inner loop for printing increasing half in each row
+#     for j in range(n,i+1):
+#         print("*",end="")   
+#      # 3rd inner loop for printing decreasing half in each row
+#     for j in range(n-1,0,-1):
+#         print("*",end="")       
+#     # print new line character for next
+#     print()    
+
+n=(int)(input("Enter the Number \n"))
+
+palindromicPyramid(n)
