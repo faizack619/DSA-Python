862ff5d244d4c4ad0cefca72c2b41d32b40c685f
faizack619 <faizack619@gmail.com>
2022-08-22 17:08:04 +0530

    PatternDone

--- DSA/pattern/basic pattern/Advances_Pattern/Star Pattern.py
+++ DSA/pattern/basic pattern/Advances_Pattern/Star Pattern.py
@@ -1,7 +1,14 @@
-    #     *
-    #    ***
-    #   *****
-
+### Star Pattern
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+#  *********
+#   *******
+#    *****
+#     ***
+#      *
 
 def palindromicPyramid(row):
     #Outer Loop for each row
@@ -17,19 +24,24 @@ def palindromicPyramid(row):
         print("*",end="")       
     # print new line character for next
     print()
-#  for i in range(0,row+1):
-#     # 1st inner loop for printing blank spaces in each row
-#     for j in range(1,row+1):
-#         print("",end=" ")
-#     # 2nd inner loop for printing increasing half in each row
-#     for j in range(n,i+1):
-#         print("*",end="")   
-#      # 3rd inner loop for printing decreasing half in each row
-#     for j in range(n-1,0,-1):
-#         print("*",end="")       
-#     # print new line character for next
-#     print()    
+
+
+ for i in range(row,0,-1):
+    # 1st inner loop for printing blank spaces in each row
+    for j in range(i,row+1):
+        print("",end=" ")
+    # 2nd inner loop for printing increasing half in each row
+    for j in range(1,i+1):
+        print("*",end="")   
+     # 3rd inner loop for printing decreasing half in each row
+    for j in range(i-1,0,-1):
+        print("*",end="")       
+    # print new line character for next
+    print()   
 
 n=(int)(input("Enter the Number \n"))
 
+
+
+print("Star pattern:")
 palindromicPyramid(n)
---
+++ DSA/pattern/basic pattern/Advances_Pattern/Zig-zigPattern.py
@@ -0,0 +1,10 @@
+
+# n=(int)(input("Enter the Number\n"))
+
+# for i in range(2):
+#     for j in range(n):
+#         if ((i+j)%4==0 ) or (i==2 and j%4==0 ):
+#             print("*",end='')
+#         else :
+#             print("-")
+#     print()            
