360e6f0e04135997703c80275793f7dda3282d24
faizack619 <faizack619@gmail.com>
2022-09-12 22:33:08 +0530

    problemSolving

---
+++ DSA/data-Structure/problemSolving/array/sumofarray.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    Reward_A,Reward_B=0
+    for i,j in zip(a,b):
+        if i>j:
+            Reward_A=Reward_A+1
+        elif i<j:
+            Reward_B=Reward_B+1
+    return(Reward_A,Reward_B)        
+        
+        
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
